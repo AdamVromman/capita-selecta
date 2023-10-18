4db553d4d5494a29333c2e32cee549679e7ec6f5
AdamVromman <adam.vromman@gmail.com>
2023-10-18 17:43:30 +0200

    finishing touch of differences

--- src/app/differences/differences.component.ts
+++ src/app/differences/differences.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
 
+const angular1List: String[] = [
+  "scopes/controllers",
+  "not as modular",
+  "not asynchronous",
+  "not on Typescript",
+  "not able to run on servers",
+  "ng-class"
+]
+const angular2List: String[] = [
+  "components",
+  "modular",
+  "asynchronous",
+  "Typescript recommended",
+  "able to run on servers",
+  "[] & ()"
+]
+
 @Component({
   selector: 'app-differences',
   templateUrl: './differences.component.html',
@@ -8,4 +25,6 @@ import { Component } from '@angular/core';
 export class DifferencesComponent {
 
   isChecked = false;
+  angular1List = angular1List;
+  angular2List = angular2List;
 }
